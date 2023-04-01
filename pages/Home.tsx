c2909f6c6c3ccb6d6db7b2a11fbd70303637921a
KevinCox0427 <kevincox0427@yahoo.com>
2023-03-31 21:11:41 -0400

    the second section

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -27,7 +27,7 @@ const Home:FunctionComponent = () => {
                     <h3><span className='Titling'>Dreams</span><span>and</span><span className='Titling'>Reality</span></h3>
                     <div className='Subtitle'>
                         <h2>Full stack developer & full graphic designer</h2>
-                        <a href='/'>Learn More</a>
+                        <a href='/#WhatICanDo'>Learn More</a>
                     </div>
                 </div>
                 <div className='IframeSlider'>
@@ -45,6 +45,16 @@ const Home:FunctionComponent = () => {
                 <svg id="RightCover" viewBox="0 0 1000 2000" xmlns="http://www.w3.org/2000/svg"><path d="M 0 0 L 0 1590 L 250 1750 L 475 1750 L 675 2000 L 2000 2000 L 2000 0 Z" fill="#eee5e4"/></svg>
                 <FloralSVG></FloralSVG>
                 <HandSVG></HandSVG>
+            </div>
+            <div id="WhatICanDo" className='Contain'>
+                <div className='NavBar'>
+                    <h2>I can</h2>
+                    <a>create optimized databases</a>
+                    <a>authenticate users</a>
+                    <a>integrate applications</a>
+                    <a>create UIs to control it all</a>
+                    <a>and beautiful websites to show it all</a>
+                </div>
             </div>
         </main>
         <Footer></Footer>
