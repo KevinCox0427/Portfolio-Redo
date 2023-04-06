5e5819cbbeee515d25070bead06862467ab78232
KevinCox0427 <kevincox0427@yahoo.com>
2023-04-05 20:33:21 -0400

    Added Leaflet

--- pages/parts/Home/IntegrationSection.tsx
+++ pages/parts/Home/IntegrationSection.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from "react";
 
-const IntegrationSection:FunctionComponent = () => {
-    if(typeof window != 'undefined') {
+declare const L:any;
+if(typeof window != 'undefined') {
+    setTimeout(() => {
         const map = new L.Map('map', {
             center: new L.LatLng(40.731253, -73.996139),
             zoom: 12,
         });
-    }
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }, 100);
+}
+
+const IntegrationSection:FunctionComponent = () => {
 
     return <div id='integrations'>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossOrigin=""/>
--- utils/watch.ts
+++ utils/watch.ts
@@ -2,62 +2,72 @@ import { resolve } from 'path';
 import { spawn, execSync, ChildProcess } from 'child_process';
 import { readdirSync, watch } from 'fs';
 
-console.log('Compiling...\n');
-execSync('tsc');
-execSync('sass styles:dist/public/css');
-execSync('webpack');
+watchScript();
 
-let isCompiling = false;
-let node = spawn('node', [resolve('dist/server.js')], {stdio: 'inherit'});
-
-watchDirectory('.', '');
-readDirectorys('.');
-console.log('Watching!\n');
-
-function watchDirectory(folder:string, pointer:string) {
-    watch(folder + '/' + pointer, (x, file) => {
-        if(pointer == 'pages' || folder.includes('pages')) {
-            runProcess('webpack', file);
-        }
-        if(file.split('.')[1] == 'scss') runProcess('sass', file);
-        if(file.split('.')[1] == 'ts' || file.split('.')[1] == 'tsx') runProcess('tsc', file);
-    });
-}
-
-function readDirectorys(folder:string) {
-    readdirSync(folder).forEach(pointer => {
-        if(pointer.includes('.') || pointer == 'node_modules' || pointer == 'dist') return;
-        watchDirectory(folder, pointer);
-        readDirectorys(folder + '/' + pointer);
-    }); 
-}
-
-function runProcess(command: string, fileName: string) {
-    if(isCompiling) return;
-    node.kill('SIGTERM');
-
-    isCompiling = true;
-    console.log('\nCompiling: ' + fileName);
-    let compiler:ChildProcess | null = null;
-    
-    switch(command){
-        case 'webpack':
-            compiler = spawn('tsc', {shell: true});
-            compiler = spawn('webpack', {shell: true});
-            break;
-        case 'sass':
-            compiler = spawn('sass', ['styles:dist/public/css'], {shell: true});
-            break;
-        case 'tsc':
-            compiler = spawn('tsc', {shell: true}).on('error', e => {console.log(e)});
-            break;
+function watchScript() {
+    console.log('Compiling...\n');
+    try {
+        execSync('tsc');
+        execSync('sass styles:dist/public/css');
+        execSync('webpack');
+    } catch (e:any) {
+        console.log(e.output.filter((err:any) => err instanceof Buffer).map((buffer:Buffer) => {
+            return `Error:\n\n${buffer.toString()}`
+        }).filter((errStr:string) => errStr.length > 'Error:\n\n '.length).join('\n'));
+        return;
     }
 
-    if(!compiler) return;
+    let compileTimestamp = Date.now();
+    let node = spawn('node', [resolve('dist/server.js')], {stdio: 'inherit'});
 
-    compiler.on('exit', () => {
-        isCompiling = false;
-        console.log('Done!\n');
-        node = spawn('node', [resolve('dist/server.js')], {stdio: 'inherit'});
-    });
+    watchDirectory('.', '');
+    readDirectorys('.');
+    console.log('Watching!\n');
+
+    function watchDirectory(folder:string, pointer:string) {
+        watch(folder + '/' + pointer, (x, file) => {
+            if(pointer == 'pages' || folder.includes('pages')) {
+                runProcess('webpack', file);
+            }
+            if(file.split('.')[1] == 'scss') runProcess('sass', file);
+            if(file.split('.')[1] == 'ts' || file.split('.')[1] == 'tsx') runProcess('tsc', file);
+        });
+    }
+
+    function readDirectorys(folder:string) {
+        readdirSync(folder).forEach(pointer => {
+            if(pointer.includes('.') || pointer == 'node_modules' || pointer == 'dist') return;
+            watchDirectory(folder, pointer);
+            readDirectorys(folder + '/' + pointer);
+        }); 
+    }
+
+    function runProcess(command: string, fileName: string) {
+        if(Date.now() - compileTimestamp < 200) return;
+        node.kill('SIGTERM');
+
+        compileTimestamp = Date.now();
+        console.log('\nCompiling: ' + fileName);
+        let compiler:ChildProcess | null = null;
+        
+        switch(command){
+            case 'webpack':
+                compiler = spawn('tsc', {shell: true});
+                compiler = spawn('webpack', {shell: true});
+                break;
+            case 'sass':
+                compiler = spawn('sass', ['styles:dist/public/css'], {shell: true});
+                break;
+            case 'tsc':
+                compiler = spawn('tsc', {shell: true}).on('error', e => {console.log(e)});
+                break;
+        }
+
+        if(!compiler) return;
+
+        compiler.on('exit', () => {
+            console.log('Done!\n');
+            node = spawn('node', [resolve('dist/server.js')], {stdio: 'inherit'});
+        });
+    }
 }
