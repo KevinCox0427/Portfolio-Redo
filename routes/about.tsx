f6b623ddee8683714e747454baac76adb564a24a
KevinCox0427 <kevincox0427@yahoo.com>
2023-05-25 16:45:21 -0400

    resume done

--- routes/about.tsx
+++ routes/about.tsx
@@ -3,14 +3,51 @@ import React from 'react';
 import portfolioConfig from '../utils/portfolioConfig';
 import serveHTML from '../utils/serveHTML';
 import About from '../pages/About';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const about = express.Router();
 
 about.route('/')
-    .get((req, res) => {
+    .get(async (req, res) => {
+        let response;
+
+        try {
+            response = (await axios.get('https://api.github.com/users/KevinCox0427/repos?sort=pushed', {
+                headers: {
+                    "Authorization": `Bearer ${process.env.GithubAPIKey}`
+                }
+            })).data;
+        }
+        catch (e) {
+            response = [{
+                owner: {
+                    avatar_url: '/assets/headshot.jpg',
+                    login: 'KevinCox0427',
+                    repos: []
+                }
+            }]
+        }
+
         const serverProps: ServerPropsType = {
             aboutPageProps: {
-                portfolioConfig: portfolioConfig
+                portfolioConfig: portfolioConfig,
+                github: {
+                    avatar: response[0].owner.avatar_url,
+                    owner: response[0].owner.login,
+                    repos: response.map((repo:any) => {
+                        return {
+                            name: repo.name,
+                            url: repo.html_url,
+                            description: repo.description,
+                            language: repo.language,
+                            topics: repo.topics,
+                            pushed: repo.pushed_at
+                        }
+                    })
+                }
             }
         }
 
